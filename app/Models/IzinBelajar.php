74c8b967222b7053bfacf71382cccceae95b98a0
Muhammad Sholeh <muhammadsholeh.dev@gmail.com>
2023-01-04 13:07:22 +0700

    feat: 🎸 models
    add models definition

---
+++ app/Models/IzinBelajar.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class IzinBelajar extends Model
+{
+    use HasFactory;
+    protected $table = "pengajuan_izin_belajar";
+    public $timestamps = false;
+    protected $guarded = ['id'];
+}
