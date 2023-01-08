46adefe31136147c0e30069a71fd28db8af11057
Muhammad Sholeh <muhammadsholeh.dev@gmail.com>
2023-01-09 00:24:31 +0700

    feat: 🎸 pengajuanku
    add and pass data to pengajuanku

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -7,6 +7,7 @@ use App\Models\Notifikasi;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\Pengajuanku;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Validator;
 
@@ -23,7 +24,9 @@ class IndexController extends Controller
     }
     public function pengajuanku()
     {
-        return view('user.pengajuanku');
+        // 0 : Menunggu, 1 : To Direktur, -1 : Ditolak, 2 : Diterima
+        $pengajuanku = Pengajuanku::whereNip(auth()->user()->nip)->get();
+        return view('user.pengajuanku', compact('pengajuanku'));
     }
     public function notifikasi()
     {
@@ -34,58 +37,15 @@ class IndexController extends Controller
         }
         return view('user.notifikasi', compact('notifikasi'));
     }
+
+
+    // PROFILE SECTION ---------------------------------------------------------------
     public function profil()
     {
         $profil = User::whereNip(auth()->user()->nip)->first();
         return view('user.profil', compact('profil'));
     }
+
     public function update_profil(Request $request)
-    {
-        $validation = Validator::make(
-            $request->all(),
-            [
-                'tempat_lahir' => 'required',
-                'tanggal_lahir' => 'required',
-                'agama' => 'required',
-                'no_telp' => 'required',
-                'email' => 'required',
-                'usia' => 'required',
-                'pas_foto' => 'required|mimes:jpg,jpeg,png|max:2048',
-                'nama' => 'required',
-                'spesialisasi' => 'required',
-                'jenjang_pendidikan' => 'required',
-                'jabatan' => 'required',
-                'pangkat_golongan' => 'required',
-            ]
-        );
-
-        if ($validation->fails()) {
-            return redirect()->back()->with('danger', $validation->errors()->all())->withInput();
-        }
-
-        $user = User::whereNip(auth()->user()->nip)->first();
-        // Delete Existing Pas Foto
-        if ($user->pas_foto) {
-            Storage::disk('public')->delete('pas_foto/' . $user->pas_foto);
-        }
-
-        if ($user->update(
-            [
-                'tempat_lahir' => $request->tempat_lahir,
-                'tanggal_lahir' => $request->tanggal_lahir,
-                'agama' => $request->agama,
-                'no_telp' => $request->no_telp,
-                'email' => $request->email,
-                'usia' => $request->usia,
-                'pas_foto' => $request->file('pas_foto')->store('images/pas_foto', 'public'),
-                'nama' => $request->nama,
-                'spesialisasi' => $request->spesialisasi,
-                'jenjang_pendidikan' => $request->jenjang_pendidikan,
-                'jabatan' => $request->jabatan,
-                'pangkat_golongan' => $request->pangkat_golongan,
-            ]
-        )) {
-            return redirect()->back()->with('success', 'Data Berhasil Diperbarui!');
-        }
-    }
+    { }
 }
--- resources/views/user/pengajuanku.blade.php
+++ resources/views/user/pengajuanku.blade.php
@@ -24,35 +24,43 @@
                 </tr>
             </thead>
             <tbody>
+                @forelse ($pengajuanku as $p)
                 <tr class="align-items-center">
-                    <th scope="row">1</th>
-                    <td>Tugas Belajar</td>
-                    <td>20 Maret 2023</td>
+                    <th scope="row">
+                        {{ $loop->iteration }}
+                    </th>
                     <td>
+                        @if($p->jenis_pengajuan == 'tb')
+                        Tugas Belajar
+                        @else
+                        Izin Belajar
+                        @endif
+                    </td>
+                    <td>{{ Carbon\Carbon::parse($p->created_updated_a)->format('l, d M Y') }}</td>
+                    <td>
+                        @if($p->status_pengajuan == 0)
                         <h5><span class="badge bg-secondary w-50">Menunggu</span></h5>
-                    </td>
-                    <td class="text-muted">Edit</td>
-                </tr>
-                <tr class="align-items-center">
-                    <th scope="row">1</th>
-                    <td>Tugas Belajar</td>
-                    <td>20 Maret 2023</td>
-                    <td>
+                        @elseif($p->status_pengajuan == 1)
+                        <h5><span class="badge bg-secondary w-50">Menunggu</span></h5>
+                        @elseif($p->status_pengajuan == 2)
                         <h5><span class="badge bg-success w-50">Diterima</span></h5>
-                    </td>
-                    <td class="text-muted">Edit</td>
-                </tr>
-                <tr class="align-items-center">
-                    <th scope="row">1</th>
-                    <td>Tugas Belajar</td>
-                    <td>20 Maret 2023</td>
-                    <td>
+                        @elseif($p->status_pengajuan == -1)
                         <h5><span class="badge bg-danger w-50">Ditolak</span></h5>
+                        @endif
                     </td>
+                    @if($p->status_pengajuan == 0 || $p->status_pengajuan == 1 || $p->status_pengajuan == 2)
+                    <td class="text-muted">Edit</td>
+                    @elseif($p->status_pengajuan == -1)
                     <td>
                         <a href="#" class="text-dark fw-bold">Edit</a>
                     </td>
+                    @endif
                 </tr>
+                @empty
+                <tr>
+                    <td colspan="5">Anda Belum Memiliki Data Pengajuan</td>
+                </tr>
+                @endforelse
             </tbody>
         </table>
     </div>
