b97a7013ca44e252b65258d2a241c58863278638
Muhammad Sholeh <muhammadsholeh.dev@gmail.com>
2023-02-04 23:04:32 +0700

    feat(TTD Direktur): init

--- app/Http/Controllers/DocumentGeneratorController.php
+++ app/Http/Controllers/DocumentGeneratorController.php
@@ -7,6 +7,8 @@ use Dompdf\Dompdf;
 use App\Models\User;
 use App\Models\Admin;
 use App\Http\Controllers\Controller;
+use App\Models\IzinBelajar;
+use App\Models\TugasBelajar;
 
 class DocumentGeneratorController extends Controller
 {
@@ -14,49 +16,56 @@ class DocumentGeneratorController extends Controller
     {
         $user = User::whereNip(auth()->user()->nip)->first();
         $admin = Admin::whereRole('2')->first();
+        $tb = TugasBelajar::whereId($id)->first();
         $time_now = Carbon::now();
 
-        return view('docs.surat_abk', compact('user', 'time_now', 'admin'));
+        return view('docs.surat_abk', compact('user', 'time_now', 'admin', 'tb'));
     }
     public function surat_rekomendasi_tubel($id)
     {
         $user = User::whereNip(auth()->user()->nip)->first();
         $admin = Admin::whereRole('2')->first();
+        $tb = TugasBelajar::whereId($id)->first();
         $time_now = Carbon::now();
-        return view('docs.surat_rekomendasi_tubel', compact('user', 'time_now', 'admin'));
+        return view('docs.surat_rekomendasi_tubel', compact('user', 'time_now', 'admin', 'tb'));
     }
     public function surat_permohonan_ib($id)
     {
         $user = User::whereNip(auth()->user()->nip)->first();
         $admin = Admin::whereRole('2')->first();
-        return view('docs.surat_permohonan_ib', compact('user', 'admin'));
+        $ib = IzinBelajar::whereId($id)->first();
+        return view('docs.surat_permohonan_ib', compact('user', 'admin', 'ib'));
     }
     public function surat_pembebasan_tb($id)
     {
         $user = User::whereNip(auth()->user()->nip)->first();
         $admin = Admin::whereRole('2')->first();
+        $tb = TugasBelajar::whereId($id)->first();
         $time_now = Carbon::now();
-        return view('docs.surat_pembebasan', compact('user', 'time_now', 'admin'));
+        return view('docs.surat_pembebasan', compact('user', 'time_now', 'admin', 'tb'));
     }
     public function ib_1($id)
     {
         $user = User::whereNip(auth()->user()->nip)->first();
         $admin = Admin::whereRole('2')->first();
+        $ib = IzinBelajar::whereId($id)->first();
         $time_now = Carbon::now();
-        return view('docs.izin-belajar.1', compact('user', 'time_now', 'admin'));
+        return view('docs.izin-belajar.1', compact('user', 'time_now', 'admin', 'ib'));
     }
     public function ib_2($id)
     {
         $user = User::whereNip(auth()->user()->nip)->first();
         $admin = Admin::whereRole('2')->first();
+        $ib = IzinBelajar::whereId($id)->first();
         $time_now = Carbon::now();
-        return view('docs.izin-belajar.2', compact('user', 'time_now', 'admin'));
+        return view('docs.izin-belajar.2', compact('user', 'time_now', 'admin', 'ib'));
     }
     public function ib_3($id)
     {
         $user = User::whereNip(auth()->user()->nip)->first();
         $admin = Admin::whereRole('2')->first();
+        $ib = IzinBelajar::whereId($id)->first();
         $time_now = Carbon::now();
-        return view('docs.izin-belajar.3', compact('user', 'time_now', 'admin'));
+        return view('docs.izin-belajar.3', compact('user', 'time_now', 'admin', 'ib'));
     }
 }
